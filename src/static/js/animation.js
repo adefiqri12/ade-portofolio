16ffbe9146128f8f622bb0cfe34dd3a4ae9e75ce
tranx00 <ade.fiqri1@gmail.com>
2025-02-06 02:19:13 +0700

    dot pattern

--- src/static/js/animation.js
+++ src/static/js/animation.js
@@ -1,25 +1,19 @@
 let lastX = window.innerWidth / 2;
 let lastY = window.innerHeight / 2;
 
-// Update position on mouse move
 document.addEventListener('mousemove', (e) => {
     lastX = e.clientX;
     lastY = e.clientY;
     updateGradientPosition();
 });
 
-// Also update on scroll
-window.addEventListener('scroll', () => {
-    updateGradientPosition();
-});
+window.addEventListener('scroll', updateGradientPosition);
 
 function updateGradientPosition() {
     const x = lastX + window.scrollX;
     const y = lastY + window.scrollY;
-    
+
     document.documentElement.style.setProperty('--x', x + 'px');
     document.documentElement.style.setProperty('--y', y + 'px');
 }
-
-// Initial position update
 updateGradientPosition();
