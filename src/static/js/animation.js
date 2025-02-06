164095a0bb4466f8b7c2466dfa86c4ff86c9b2bd
adefiqri12 <ade.fiqri1@gmail.com>
2025-02-07 00:13:22 +0700

    test color

--- src/static/js/animation.js
+++ src/static/js/animation.js
@@ -12,8 +12,10 @@ window.addEventListener('scroll', updateGradientPosition);
 function updateGradientPosition() {
     const x = lastX + window.scrollX;
     const y = lastY + window.scrollY;
-
+    
     document.documentElement.style.setProperty('--x', x + 'px');
     document.documentElement.style.setProperty('--y', y + 'px');
 }
+
+// Initialize gradient position
 updateGradientPosition();
